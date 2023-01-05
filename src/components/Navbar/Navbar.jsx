26cde43a100376527be0b5ae4d7c350f91ceddd3
Mara Scampini <109754827+MaraScampini@users.noreply.github.com>
2023-01-05 10:09:32 +0100

    [Fix] Fixes to style in login and register and add logo to navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,23 +2,26 @@ import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import { useNavigate } from "react-router-dom";
+import logo from "../../assets/gym-near-svgrepo-com.svg";
 
 const NavbarHeader = () => {
   const navigate = useNavigate();
   return (
-    <Navbar collapseOnSelect expand="lg" className="header" >
+    <Navbar collapseOnSelect expand="lg" className="header">
       <Container fluid>
-        <Navbar.Brand href="#home">LOGO</Navbar.Brand>
+        <Navbar.Brand className="headerLink" onClick={() => navigate("/")}>
+          <img src={logo} width="35" />
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto">
-            <Nav>Routines</Nav>
-            <Nav>Exercises</Nav>
+            <Nav className="mx-3 headerLink">Routines</Nav>
+            <Nav className="mx-3 headerLink">Exercises</Nav>
           </Nav>
           <Nav>
-            <Nav
-            onClick={()=>navigate("/login")}
-            >Log In</Nav>
+            <Nav className="headerLink mx-3" onClick={() => navigate("/login")}>
+              Log In
+            </Nav>
           </Nav>
         </Navbar.Collapse>
       </Container>
