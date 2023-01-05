5b1dbd75c2e38bfaac8aad89d8892eaa9a91a9f1
Mara Scampini <109754827+MaraScampini@users.noreply.github.com>
2023-01-05 09:27:38 +0100

    [Feat] Added second password check to component and function to clear inputs when navigating between login and register

--- src/containers/Login/Login.jsx
+++ src/containers/Login/Login.jsx
@@ -1,9 +1,10 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { errorCheck } from "../../services/Useful";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import { userLogin, userRegister } from "../../services/ApiCalls";
+import { Col, Container, Row } from "react-bootstrap";
 
 const Authentication = ({ type }) => {
   const navigate = useNavigate();
@@ -12,43 +13,80 @@ const Authentication = ({ type }) => {
     email: "",
     password: "",
     username: "",
+    password2: "",
   });
   const [userError, setUserError] = useState({
     emailError: "",
     passwordError: "",
     empty: "",
     wrongCredentials: "",
+    usernameError: "",
+    password2Error: "",
+    emailAlreadyInBBDD: "",
+  });
+  const [input, setInput] = useState({
+    email: "",
+    password: "",
   });
 
   let body = {
     email: user.email,
     password: user.password,
     username: user.username,
+    password2: user.password2,
   };
 
   const validateBody = (body) => {
-    if (type == "login") {
+    if (type === "login") {
       if (body.email !== "" && body.password !== "") {
         return true;
       }
-    } else if (type == "register") {
-      if (body.email !== "" && body.password !== "" && body.username !== "") {
+    } else if (type === "register") {
+      if (
+        body.email !== "" &&
+        body.password !== "" &&
+        body.username !== "" &&
+        body.password2 !== "" &&
+        body.password === body.password2
+      ) {
         return true;
       }
     }
   };
 
   const isRegister = () => {
-    if (type == "register") {
+    if (type === "register") {
       return true;
-    } else if (type == "login") {
+    } else if (type === "login") {
       return false;
     }
   };
 
+  const cleanStates = () => {
+    setUser(() => ({
+      email: "",
+      password: "",
+      username: "",
+      password2: "",
+    }));
+    setInput(() => ({
+      email: "",
+      password: "",
+    }));
+  };
+
+  const navigateLogin = () => {
+    cleanStates();
+    navigate(`/login`);
+  };
+    const navigateRegister = () => {
+      cleanStates();
+      navigate(`/register`);
+    };
+
   const submitLogin = (e) => {
     e.preventDefault();
-    if (validateBody(body) && type == "login") {
+    if (validateBody(body) && type === "login") {
       userLogin(body)
         .then(() => navigate("/"))
         .catch((error) => {
@@ -58,16 +96,21 @@ const Authentication = ({ type }) => {
             empty: "",
           }));
         });
-    } else if (validateBody(body) && type == "register") {
+    } else if (validateBody(body) && type === "register") {
       userRegister(body)
-        .then(() => navigate("/"))
+        .then((login) => console.log(login))
         .catch((error) => {
           setUserError((prevState) => ({
             ...prevState,
-            wrongCredentials: error.response.data.message,
+            emailAlreadyInBBDD: error.response.data,
             empty: "",
           }));
         });
+    } else if (user.password !== user.password2) {
+      setUserError((prevState) => ({
+        ...prevState,
+        password2Error: "Passwords don't match",
+      }));
     } else {
       setUserError((prevState) => ({
         ...prevState,
@@ -76,11 +119,14 @@ const Authentication = ({ type }) => {
     }
   };
 
+
+
   const inputHandler = (e) => {
     setUser((prevState) => ({
       ...prevState,
       [e.target.name]: e.target.value,
     }));
+    setInput(e.target.value);
   };
 
   const errorHandler = (field, value, type, password) => {
@@ -92,76 +138,114 @@ const Authentication = ({ type }) => {
     }));
   };
 
+
+
   return (
-    <div>
-      <Form onSubmit={(e) => submitLogin(e)}>
-        <div>{userError.empty}</div>
-        <div>{userError.wrongCredentials}</div>
-        {isRegister() ? (
-          <Form.Group className="mb-3">
-            <Form.Label>Username</Form.Label>
-            <Form.Control
-              type="text"
-              name="username"
-              placeholder="Enter your username"
-              onChange={(e) => inputHandler(e)}
-              onBlur={(e) =>
-                errorHandler(e.target.name, e.target.value, "username")
-              }
-            />
-          </Form.Group>
-        ) : (
-          <div></div>
-        )}
-        <Form.Group className="mb-3" controlId="formBasicEmail">
-          <Form.Label>Email address</Form.Label>
-          <Form.Control
-            type="email"
-            name="email"
-            placeholder="Enter email"
-            onChange={(e) => inputHandler(e)}
-            onBlur={(e) => errorHandler(e.target.name, e.target.value, "email")}
-          />
-          <Form.Text className="text-muted">
-            We'll never share your email with anyone else.
-          </Form.Text>
-          <div className="errorInput">{userError.emailError}</div>
-        </Form.Group>
+    <Container>
+      <Row className="d-flex justify-content-center mt-sm-5">
+        <Col xs="11" md="7">
+          <Form onSubmit={(e) => submitLogin(e)}>
+            <div className="errorInput">{userError.empty}</div>
+            <div className="errorInput">{userError.wrongCredentials}</div>
+            <div className="errorInput">{userError.emailAlreadyInBBDD}</div>
+            {isRegister() ? (
+              <Form.Group className="mb-3">
+                <Form.Label>Username</Form.Label>
+                <Form.Control
+                  type="text"
+                  name="username"
+                  placeholder="Enter your username"
+                  onChange={(e) => inputHandler(e)}
+                  onBlur={(e) =>
+                    errorHandler(e.target.name, e.target.value, "username")
+                  }
+                />
+                <div className="errorInput">{userError.usernameError}</div>
+              </Form.Group>
+            ) : (
+              <div></div>
+            )}
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control
+                type="email"
+                name="email"
+                placeholder="Enter email"
+                value={input.email}
+                onChange={(e) => inputHandler(e)}
+                onBlur={(e) =>
+                  errorHandler(e.target.name, e.target.value, "email")
+                }
+              />
+              <Form.Text className="text-muted">
+                We'll never share your email with anyone else.
+              </Form.Text>
+              <div className="errorInput">{userError.emailError}</div>
+            </Form.Group>
 
-        <Form.Group className="mb-3" controlId="formBasicPassword">
-          <Form.Label>Password</Form.Label>
-          <Form.Control
-            type="password"
-            name="password"
-            placeholder="Password"
-            onChange={(e) => inputHandler(e)}
-            onBlur={(e) =>
-              errorHandler(e.target.name, e.target.value, "password")
-            }
-          />
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control
+                type="text"
+                name="password"
+                placeholder="Password"
+                value={input.password}
+                onChange={(e) => inputHandler(e)}
+                onBlur={(e) =>
+                  errorHandler(e.target.name, e.target.value, "password")
+                }
+              />
+              {isRegister() ? (
+                <div className="errorInput">{userError.passwordError}</div>
+              ) : (
+                <div></div>
+              )}
+            </Form.Group>
+            {isRegister() ? (
+              <Form.Group className="mb-3">
+                <Form.Label>Repeat password</Form.Label>
+                <Form.Control
+                  type="text"
+                  name="password2"
+                  placeholder="Enter your password"
+                  onChange={(e) => inputHandler(e)}
+                  onBlur={(e) =>
+                    errorHandler(
+                      e.target.name,
+                      e.target.value,
+                      "password2",
+                      user.password
+                    )
+                  }
+                />
+                <div className="errorInput">{userError.password2Error}</div>
+              </Form.Group>
+            ) : (
+              <div></div>
+            )}
+
+            <Button id="authButton" type="submit">
+              Submit
+            </Button>
+          </Form>
           {isRegister() ? (
-            <div className="errorInput">{userError.passwordError}</div>
+            <div className="authText">
+              <p>Already have an account?</p>
+              <p className="authLink" onClick={() => navigateLogin()}>
+                Login
+              </p>
+            </div>
           ) : (
-            <div></div>
+            <div className="authText">
+              <p>Do not have an account?</p>
+              <p className="authLink" onClick={() => navigateRegister()}>
+                Register
+              </p>
+            </div>
           )}
-        </Form.Group>
-
-        <Button variant="primary" type="submit">
-          Submit
-        </Button>
-      </Form>
-      {isRegister() ? (
-        <div>
-          <p>Already have an account?</p>
-          <p onClick={() => navigate("/login")}>Login</p>
-        </div>
-      ) : (
-        <div>
-          <p>Do not have an account?</p>
-          <p onClick={() => navigate("/register")}>Register</p>
-        </div>
-      )}
-    </div>
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
