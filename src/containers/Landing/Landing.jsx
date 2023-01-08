4b7dd77c38caf22dc38a8498d31515950ce54b26
Mara Scampini <109754827+MaraScampini@users.noreply.github.com>
2023-01-08 18:00:19 +0100

    [Feat] Change buttons in landing page depending on the user being logged in

--- src/containers/Landing/Landing.jsx
+++ src/containers/Landing/Landing.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import { Button, Col, Container, Row } from 'react-bootstrap'
-import { useNavigate } from 'react-router-dom';
-import hero from '../../assets/hero-image.jpg'
+import React, { useEffect, useState } from "react";
+import { Button, Col, Container, Row } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
 
 function Landing() {
   const navigate = useNavigate();
+  const token = localStorage.getItem("jwt");
+
+  const [loggedIn, setLoggedIn] = useState(false);
+
+  useEffect(() => {
+    token ? setLoggedIn(true) : setLoggedIn(false);
+  }, [token]);
+
   return (
     <Container fluid className="landingContainer">
       <Row>
@@ -17,26 +24,39 @@ function Landing() {
           <div className="text-center padding">
             <p className="landingText">Consistence has never been easier</p>
           </div>
-          <Button
-            className="mt-3 mt-sm-5 lifterButton"
-            onClick={() => navigate("/register")}
-          >
-            REGISTER
-          </Button>
-          <div className="lifterDownText">
-            <p> Already have an account?</p>
-            <p
-              className="lifterLink whiteText"
-              onClick={() => navigate("/login")}
-            >
-              {" "}
-              Login
-            </p>
-          </div>
+          {loggedIn ? (
+            <div>
+              <Button
+                className="mt-3 mt-sm-5 lifterButton"
+                onClick={() => navigate("/routines")}
+              >
+                ROUTINES
+              </Button>
+            </div>
+          ) : (
+            <div className="d-flex flex-column align-items-center">
+              <Button
+                className="mt-3 mt-sm-5 lifterButton"
+                onClick={() => navigate("/register")}
+              >
+                start now
+              </Button>
+              <div className="lifterDownText">
+                <p> Already have an account?</p>
+                <p
+                  className="lifterLink whiteText"
+                  onClick={() => navigate("/login")}
+                >
+                  {" "}
+                  Login
+                </p>
+              </div>
+            </div>
+          )}
         </Col>
       </Row>
     </Container>
   );
 }
 
-export default Landing
+export default Landing;
